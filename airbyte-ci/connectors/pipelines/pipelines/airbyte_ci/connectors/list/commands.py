10d5ab40158c7ab0646eea5eb59d41537d2ffeff
alafanechere <augustin.lafanechere@gmail.com>
2024-10-29 23:51:41 +0100

    airbyte-ci: batch connector for up-to-date by chunks of 50

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/list/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/list/commands.py
@@ -2,6 +2,9 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import json
+import pathlib
+
 import asyncclick as click
 from connector_ops.utils import console  # type: ignore
 from pipelines.cli.dagger_pipeline_command import DaggerPipelineCommand
@@ -10,9 +13,11 @@ from rich.text import Text
 
 
 @click.command(cls=DaggerPipelineCommand, help="List all selected connectors.", name="list")
+@click.option("--output-file", type=click.Path(exists=False, writable=True, path_type=pathlib.Path), help="Output file path.", default=None)
 @click.pass_context
 async def list_connectors(
     ctx: click.Context,
+    output_file: pathlib.Path,
 ) -> bool:
     selected_connectors = sorted(ctx.obj["selected_connectors_with_modified_files"], key=lambda x: x.technical_name)
     table = Table(title=f"{len(selected_connectors)} selected connectors")
@@ -23,7 +28,18 @@ async def list_connectors(
     table.add_column("Version")
     table.add_column("Folder")
 
+
+    connector_dicts = []
     for connector in selected_connectors:
+        connector_dicts.append({
+            "name": connector.technical_name,
+            "language": connector.language.value if connector.language else None,
+            "support_level": connector.support_level if connector.support_level else None,
+            "version": connector.version if connector.version else None,
+            "folder": str(connector.code_directory),
+            "modified": bool(connector.modified_files),  
+        })
+        
         modified = "X" if connector.modified_files else ""
         connector_name = Text(connector.technical_name)
         language: Text = Text(connector.language.value) if connector.language else Text("N/A")
@@ -39,4 +55,6 @@ async def list_connectors(
         table.add_row(modified, connector_name, language, support_level, version, folder)
 
     console.print(table)
+    output_file.write_text(json.dumps(connector_dicts, indent=2))
+    console.print(f"List of selected connectors written to {output_file}")
     return True
